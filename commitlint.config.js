5f2efb80448a3462e347158e545efedecdbee1cb
michael-cotton-nttd <Michael11.Cotton@nttdata.com>
2025-02-13 20:30:22 +0000

    chore: remove commented code in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,3 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    // rules: {
-    //   'type-enum': [2, 'always', [
-    //     'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'
-    //   ]],
-    //   'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    // },
 };
